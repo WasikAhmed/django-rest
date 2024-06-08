c285e19047b92c8b771c1d03be35a849ca5b979d
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-08 14:29:08 +0600

    Add custom user model

--- simple_jwt_auth/simple_jwt_auth/settings.py
+++ simple_jwt_auth/simple_jwt_auth/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'api',
+    'account',
     'rest_framework',
 ]
 
@@ -123,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+####
+AUTH_USER_MODEL = 'account.UserData'
